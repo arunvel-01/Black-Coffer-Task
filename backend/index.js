be6983868e61e6bd27476199ae28ace0cd90b58b
Arun Vel <arunvelv9253@gmail.com>
2024-08-01 22:11:29 +0530

    Added morgan

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,13 @@ import mongoose from 'mongoose';
 import dotenv from "dotenv";
 import cors from "cors";
 import { job } from './cron.js';
+import morgan from 'morgan';
 
 const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(cors());
+app.use(morgan('tiny'));  
 
 const mongoURI = process.env.MONGODB_URI;
 mongoose.connect(mongoURI);
